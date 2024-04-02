8764d9c05b5a437a5dbc32d8e3e129fb2f6b4876
stepney141 <stepney141andi.c.i@gmail.com>
2024-04-02 10:43:26 +0900

    様々な設定変更の反映

--- dot_config/i3/executable_monitor_detect.sh
+++ dot_config/i3/executable_monitor_detect.sh
@@ -5,6 +5,7 @@
 
 st_prev="disconnected"
 thinkpad_x13_embedded_display="ELAN901C:00 04F3:2FE6"
+# hdmi_name=$(xrandr --current | awk '/HDMI-A-[0-2] connected/{print $1}')
 hdmi_name="HDMI-A-1"
 
 while :
@@ -13,11 +14,10 @@ do
     if [ $st_prev != $st_next ]; then
         st_prev=$st_next
         if [ $st_next = "connected" ]; then
-            # xrandr --output eDP --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output HDMI-A-0 --mode 1920x1200 --pos 0x0 --rotate normal
-            xrandr --output eDP --primary --auto --output $hdmi_name --auto --left-of eDP
+            xrandr --output eDP --primary --auto --output "HDMI-A-0" --auto --left-of eDP
         else
             ## 外部ディスプレイを接続していない時の設定
-            xrandr --output eDP --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output $hdmi_name --off
+            xrandr --output eDP --primary --mode 1920x1200 --pos 1920x0 --rotate normal --output "HDMI-A-0" --off
         fi
     fi
      xinput --map-to-output $(xinput list --id-only $thinkpad_x13_embedded_display) eDP
--- dot_wezterm.lua
+++ dot_wezterm.lua
@@ -11,7 +11,7 @@ if wezterm.config_builder then
 end
 
 -- This is where you actually apply your config choices
-config.color_scheme = 'PhD (base16)'
+config.color_scheme = 'Guezwhoz'
 
 config.window_background_opacity = 0.8
 config.text_background_opacity = 1.0
@@ -21,9 +21,11 @@ config.enable_tab_bar = false
 config.font_size = 10.5
 config.cell_width = 0.9
 config.font = wezterm.font_with_fallback {
-  'Noto Sans Mono', 'UDEV Gothic NF', 'Terminess Nerd Font Mono', 'MesloLGS NF', 'Noto Emoji', 'Symbola',
+  'Noto Sans Mono', 'UDEV Gothic NF', 'Terminess Nerd Font Mono', 'MesloLGS NF', 'Noto Emoji', 'Symbola', 'Symbols Nerd Font'
 }
 
+config.use_ime = true
+config.ime_preedit_rendering = "Builtin"
 
 -- and finally, return the configuration to wezterm
 return config
