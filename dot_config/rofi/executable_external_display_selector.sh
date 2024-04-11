270e1096b5bdfe19a4016dd1cda32144a33e14da
stepney141 <stepney141andi.c.i@gmail.com>
2024-04-11 18:29:29 +0900

    xrandrスクリプト変更

--- dot_config/rofi/executable_external_display_selector.sh
+++ dot_config/rofi/executable_external_display_selector.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 list=(
-  "Mirroring eDP" "xrandr --output eDP --primary --auto --rotate normal"
-  "HDMI"          "xrandr --output eDP --primary --auto --rotate normal --output HDMI-A-0 --auto --above eDP"
-  "Type-C No1"    "xrandr --output eDP --primary --auto --rotate normal --output DisplayPort-0 --auto --above eDP"
-  "Type-C No2"    "xrandr --output eDP --primary --auto --rotate normal --output DisplayPort-1 --auto --above eDP"
+  "eDP"           "xrandr --output eDP --primary --auto --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off"
+  "HDMI"          "xrandr --output eDP --primary --auto --rotate normal --output HDMI-A-0 --auto --above eDP --output DisplayPort-0 --off --output DisplayPort-1 --off"
+  "Type-C No1"    "xrandr --output eDP --primary --auto --rotate normal --output DisplayPort-0 --auto --above eDP --output HDMI-A-0 --off --output DisplayPort-1 --off"
+  "Type-C No2"    "xrandr --output eDP --primary --auto --rotate normal --output DisplayPort-1 --auto --above eDP --output HDMI-A-0 --off --output DisplayPort-0 --off "
 )
 
 if selected="$(for (( i=0; i<=$((${#list[@]}/2-1)); i++ )) { echo "${list[($i+1)*2-2]}"; } | rofi -dmenu -i -format i)"; then
