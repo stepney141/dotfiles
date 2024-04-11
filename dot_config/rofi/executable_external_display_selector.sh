236297abe7255568146855d5a5161e7e5c488cbf
stepney141 <stepney141andi.c.i@gmail.com>
2024-04-11 18:24:04 +0900

    xrandrスクリプト変更

--- dot_config/rofi/executable_external_display_selector.sh
+++ dot_config/rofi/executable_external_display_selector.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 list=(
-  "eDP"        "xrandr --output eDP --primary --auto --pos 0x0 --rotate normal"
-  "HDMI"       "xrandr --output eDP --primary --auto --pos 0x0 --rotate normal --output HDMI-A-0 --auto --above eDP"
-  "Type-C No1" "xrandr --output eDP --primary --auto --rotate normal --output DisplayPort-0 --auto --above eDP"
-  "Type-C No2" "xrandr --output eDP --primary --auto --rotate normal --output DisplayPort-0 --auto --above eDP"
+  "Mirroring eDP" "xrandr --output eDP --primary --auto --rotate normal"
+  "HDMI"          "xrandr --output eDP --primary --auto --rotate normal --output HDMI-A-0 --auto --above eDP"
+  "Type-C No1"    "xrandr --output eDP --primary --auto --rotate normal --output DisplayPort-0 --auto --above eDP"
+  "Type-C No2"    "xrandr --output eDP --primary --auto --rotate normal --output DisplayPort-1 --auto --above eDP"
 )
 
 if selected="$(for (( i=0; i<=$((${#list[@]}/2-1)); i++ )) { echo "${list[($i+1)*2-2]}"; } | rofi -dmenu -i -format i)"; then
