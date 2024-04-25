f2979ad838e980e0d625bd7141ad458cad9075fb
stepney141 <stepney141andi.c.i@gmail.com>
2024-04-25 10:57:09 +0900

    自動設定スクリプトを常時起動化

--- dot_config/executable_external-keyboard.sh
+++ dot_config/executable_external-keyboard.sh
@@ -3,6 +3,11 @@
 # ref: https://socinuit.hatenablog.com/entry/2021/04/12/194516
 
 keyboard_name="keyboard:NuPhy Air75 V2-1 Keyboard"
-keyboard_id=$(xinput list --id-only "$keyboard_name")
 
-setxkbmap -device "$keyboard_id" -layout us
+while :
+do
+    keyboard_id=$(xinput list --id-only "$keyboard_name")
+    setxkbmap -device "$keyboard_id" -layout us
+    sleep 1
+done
+
